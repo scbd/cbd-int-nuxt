0846cfc531d71c87a8028bff455cc0af19aa8250
Rodrigo Elias <rodrigo.elias@cbd.int>
2025-03-18 17:12:11 -0400

    fix environment variables.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,13 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     '@nuxtjs/i18n'
   ],
-
+  runtimeConfig: {
+    DRUPAL_CLIENT_SECRET: process.env.DRUPAL_CLIENT_SECRET,
+    public: {
+      DRUPAL_URL: process.env.DRUPAL_URL,
+      DRUPAL_CLIENT_ID: process.env.DRUPAL_CLIENT_ID,
+      DRUPAL_SCOPE: process.env.DRUPAL_SCOPE
+    }
+  },
   css: ['~/assets/scss/styles.scss'],
 })
