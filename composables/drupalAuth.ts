278872c3c8eb3754a92f5c3b9891cd57b2147386
Rodrigo Elias <rodrigo.elias@cbd.int>
2025-03-18 17:11:37 -0400

    OAuth token fixes

--- composables/drupalAuth.ts
+++ composables/drupalAuth.ts
@@ -1,45 +1,48 @@
 export const getDrupalToken = async () => { 
-    const params = new URLSearchParams()
-    params.append('grant_type', 'client_credentials')
-    params.append('client_id', process.env.DRUPAL_CLIENT_ID as string)
-    params.append('client_secret', process.env.DRUPAL_CLIENT_SECRET as string)
-    params.append('scope', process.env.DRUPAL_SCOPE as string)
+  const config = useRuntimeConfig();    
+  
+  const params = new URLSearchParams()
+  params.append('grant_type', 'client_credentials')
+  params.append('client_id', config.public.DRUPAL_CLIENT_ID)
+  params.append('client_secret', config.DRUPAL_CLIENT_SECRET)
+  params.append('scope', config.public.DRUPAL_SCOPE)
 
-    const response = await fetch(`${process.env.DRUPAL_URL}/oauth/token`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: params,
-    });
+  const response = await fetch(`${config.public.DRUPAL_URL}/oauth/token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: params,
+  });
 
-    if (response.ok) {
-        const data = await response.json()
-        data.timeout = Date.now() + (data.expires_in * 1000)
-        return data
-    } else {
-        throw new Error(`Error: ${response.status}`)
-    }
+  if (response.ok) {
+    const data = await response.json()
+    data.timeout = Date.now() + (data.expires_in * 1000)
+    return data
+  } else {
+    throw new Error(`Error: ${response.status}`)
+  }
 }
 
 export const checkDrupalToken = async (token: string | null) => {
-    if(!token) return 'invalid'
+  if(!token) return 'invalid'
 
-    const credentials = Buffer.from(`${process.env.DRUPAL_CLIENT_ID}:${process.env.DRUPAL_CLIENT_SECRET}`).toString('base64');
-    const response = await fetch(`${process.env.DRUPAL_URL}/oauth/tokens/${token}`, {
-        method: 'GET',
-        headers: {
-          'Authorization': `Basic ${credentials}`
-        }
-      });
-
-      if (response.ok) {
-        
-        const data = await response.json()
-        console.log(data)
-
-        return data
-      } else {
-        throw new Error(`Error: ${response.status}`)
+  const config = useRuntimeConfig();
+  
+  const credentials = Buffer.from(`${config.public.DRUPAL_CLIENT_ID}:${config.DRUPAL_CLIENT_SECRET}`).toString('base64');
+  const response = await fetch(`${config.public.DRUPAL_URL}/oauth/tokens/${token}`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Basic ${credentials}`
       }
+    });
+
+    if (response.ok) {
+      const data = await response.json()
+      console.log(data)
+
+      return data
+    } else {
+      throw new Error(`Error: ${response.status}`)
+    }
 }
--- middleware/drupalAuth.global.ts
+++ middleware/drupalAuth.global.ts
@@ -1,13 +1,13 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const drupalToken = useState<drupalToken>('drupal_token', () => null)
   
-  const tokenExpired = drupalToken.value && drupalToken.value.timeout && drupalToken.value.timeout <= Date.now()
+  const tokenExpired = drupalToken && drupalToken.timeout && drupalToken.timeout <= Date.now()
 
   if (!drupalToken.value || tokenExpired) {
     try {
       const tokenData = await getDrupalToken()
-      drupalToken.value = tokenData
-      console.log('Drupal authentication successful, token valid until:', new Date(tokenData.timeout).toLocaleString())
+      drupalToken.value = tokenData;
+      console.log('Token valid until:', new Date(tokenData.timeout).toLocaleString())
     } catch (error) {
       console.error('Error:', error)
     }
