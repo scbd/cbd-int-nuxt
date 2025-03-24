41f6b199ef9db25a81df21bb7702baed4f6f966e
RomeoSaundersII <romeo.saunders@gmail.com>
2025-03-24 10:21:01 -0400

    Added error animation

--- composables/langDom.ts
+++ composables/langDom.ts
@@ -32,16 +32,21 @@ export const languageChange = (
     current_lang_button: Element | null,
     language_selector_dropdown: Element | null
 ) => {
+<<<<<<< HEAD
     const user_language_settings: { active_language: string } | null = useState('user_settings').value as { active_language: string };
     const user_language: string = user_language_settings?.active_language || 'en';
     
     getDrupalLanguages(user_language)
+=======
+    getDrupalLanguages(current_language)
+>>>>>>> 36f2a75 (Added error animation)
         .catch((error) => {
             console.error(error);
             loader?.classList.add('error-loader');
         })
         .then((languages) => {
             drupal_languages.splice(0, drupal_languages.length);
+<<<<<<< HEAD
             drupal_languages.push(...languages);
             loader?.classList.remove('show-loader');
         })
@@ -50,5 +55,18 @@ export const languageChange = (
             drupal_languages.forEach((language, index) => {
                 langDropdownBuilder(language, user_language, current_lang_button, language_selector_dropdown);
             })
+=======
+            drupal_languages.push(...languages);            
+        })
+        .finally(() => {
+            // Added pause to confirm loader is working
+            setTimeout(() => {
+                language_selector_dropdown!.innerHTML = '';
+                drupal_languages.forEach((language, index) => {
+                    langDropdownBuilder(language, current_language, current_lang_button, language_selector_dropdown);
+                })                
+                loader?.classList.remove('show-loader');
+            }, 1000);
+>>>>>>> 36f2a75 (Added error animation)
         })
 }
