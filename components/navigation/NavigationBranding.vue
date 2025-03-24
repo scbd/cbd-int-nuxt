4ce6e9daa6ef14edba688583058f2382273ad53c
RomeoSaundersII <romeo.saunders@gmail.com>
2025-03-24 13:54:16 -0400

    Updated to useState()

--- components/navigation/NavigationBranding.vue
+++ components/navigation/NavigationBranding.vue
@@ -1,29 +1,7 @@
-<script setup lang="ts">
-
-onMounted(() => {
-    const logo = document.querySelector('.cus-logo');
-    let active_lang: string = document.querySelector('html')!.getAttribute('lang') || 'en';
-    
-    // Move to Composable?
-    // TODO: Load correct image on first arrival
-    const language_observer = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-            if (mutation.type === 'attributes') {
-                active_lang = document.querySelector('html')?.getAttribute('lang') || 'en';
-                logo?.setAttribute('src', `/images/CBD-logo-${active_lang.includes('-') ? active_lang.replace('-hans','').toLocaleUpperCase() : active_lang.toLocaleUpperCase()}.png`);
-            }
-        })
-    })
-
-    language_observer.observe(document.querySelector('html')!, { attributes: true });
-})
-
-    
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
     <NuxtLink to="/" class="navbar-brand">
-        <img class="cus-logo" src="/images/logo_cbd_green.svg" alt="Convention of Biological Diversity Logo" />
+        <img class="cus-logo" src="/images/logo_cbd_green.svg" alt="Convention of Biological Diversity Logo" rel="preload" />
     </NuxtLink>
 </template>
--- components/navigation/NavigationOptions.vue
+++ components/navigation/NavigationOptions.vue
@@ -1,3 +1,102 @@
+<script setup lang="ts">
+onMounted(() => {
+    let active_lang: string = document.querySelector('html')!.getAttribute('lang') || 'en';
+    
+    // TODO: Consider detecting the user's browser language and matching it to available languages
+    // If present, set language to browser, otherwise default lang attribute or a cookie
+    const navigator_languages = navigator?.languages;
+
+    const current_lang_button: Element | null = document.querySelector('.nav-options-current-lang-slot');
+    const language_selector_dropdown: Element | null = document.querySelector('.language-selector-dropdown');
+
+    // Loader
+    const loader: Element | null = document.querySelector('.loader-wrapper');
+
+    // const nav_options_links: NodeList | null = document.querySelectorAll('[class^=nav-options-]');
+
+    const drupal_languages: { langCode: string, label: string, direction: string }[] = [];
+
+    
+    languageChange(drupal_languages, loader, current_lang_button, language_selector_dropdown);
+    
+    const language_observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            if (mutation.type === 'attributes') {
+                languageChange(drupal_languages, loader, current_lang_button, language_selector_dropdown);
+            }
+        })
+    })
+
+    language_observer.observe(document.querySelector('html')!, { attributes: true });
+    
+    
+    /*
+    ///////// Ignore this mess. It's been migrated to langDom.ts ////////
+    
+    const langChange = async (current_language: string) => {
+        getDrupalLanguages(current_language)
+            .then((languages) => {
+                setTimeout(() => {
+                    console.log('Getting Languages...')
+                    drupal_languages.splice(0, drupal_languages.length);
+                    drupal_languages.push(...languages);
+                    // loader?.classList.toggle('show-loader');
+                }, 1000);
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+            .finally(() => {
+                setTimeout(() => {
+                    language_selector_dropdown!.innerHTML = '';
+                    drupal_languages.forEach((language, index) => {
+                        langDropdownBuilder(language);
+                    })                
+                    // loader?.classList.toggle('show-loader');
+                    console.log('Languages Received!');
+                }, 1000);
+            })
+    }
+
+    const langDropdownBuilder = (language: { langCode: string, label: string, direction: string }) => {
+        
+        if (language.langCode === active_lang) {
+            current_lang_button!.innerHTML = language.label;
+        } else {
+            const list_item: Element | null = document.createElement('li');
+            const anchor: Element | null = document.createElement('a');
+
+            anchor?.classList.add('dropdown-item');
+            anchor?.setAttribute('data-lang-code', language.langCode);
+            anchor?.setAttribute('href', '#');
+            anchor?.addEventListener('click', (event) => {
+                setLanguage(language.langCode);
+                active_lang = language.langCode;
+                event.stopPropagation();
+            });
+            anchor!.innerHTML = language.label;                
+            list_item?.appendChild(anchor);
+            language_selector_dropdown?.appendChild(list_item);
+        }
+    }    
+
+    const language_observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            if (mutation.type === 'attributes') {
+                console.log(mutation.attributeName);
+                langChange(active_lang);
+            }
+        });
+    })
+
+    langChange(active_lang);
+
+    language_observer.observe(document.querySelector('html')!, { attributes: true});
+
+    */
+});
+
+</script>
 <script setup lang="ts"></script>
 
 <template>
