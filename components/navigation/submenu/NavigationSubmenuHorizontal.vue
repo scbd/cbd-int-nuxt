c44e7f355d0bc3e6a245eb76c087318f7c7f918b
RomeoSaundersII <romeo.saunders@gmail.com>
2025-05-27 12:08:28 -0400

    Fixed horizontal submenu.

--- components/navigation/submenu/NavigationSubmenuHorizontal.vue
+++ components/navigation/submenu/NavigationSubmenuHorizontal.vue
@@ -4,89 +4,94 @@ import type { fetchedMenuItem } from "~/types/drupalMenu";
 const route = useRoute();
 
 const props = defineProps<{
-  submenuName: string;
+  submenuItems: fetchedMenuItem[];
 }>();
-await handlerSubmenuNavigation(props.submenuName);
-
-const submenuItems = ref<fetchedMenuItem[]>([]);
 
 const currentPath = route.path;
-for await (const menuItem of submenu.value) {
-  for (const childItem of menuItem.children) {
-    if (currentPath.includes(childItem.link)) {
-      submenuItems.value.push(menuItem);
-    }
-  }
-}
+const fullPath = route.fullPath;
 </script>
 <template>
   <div class="protocol-subnavigation accent-cbd">
     <nav class="navbar container-xxl">
       <ClientOnly>
-        <ul class="nav" v-for="submenuItem in submenuItems">
-          <div class="subnav-header">
-            <li class="nav-item selected">
-              <NuxtLink
-                :to="submenuItem.link === '<nolink>' ? '' : submenuItem.link"
-                class="nav-link"
-              >
-                {{ submenuItem.title }}
-              </NuxtLink>
-            </li>
-            <li class="nav-item">
-              <button
-                class="btn cbd-btn-subnavigation"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseSubnav"
-                aria-expanded="true"
-                aria-controls="collapseSubnav"
-              >
-                <svg
-                  width="14"
-                  height="8"
-                  viewBox="0 0 14 8"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
+        <template v-for="submenuItem in submenuItems">
+          <ul class="nav">
+            <div class="subnav-header">
+              <li class="nav-item selected">
+                <NuxtLink
+                  :to="submenuItem.link === '<nolink>' ? '' : submenuItem.link"
+                  class="nav-link"
                 >
-                  <path
-                    d="M12.92 0.180054H6.68999H1.07999C0.119992 0.180054 -0.360007 1.34005 0.319993 2.02005L5.49999 7.20005C6.32999 8.03005 7.67999 8.03005 8.50999 7.20005L10.48 5.23005L13.69 2.02005C14.36 1.34005 13.88 0.180054 12.92 0.180054Z"
-                  />
-                </svg>
-              </button>
-            </li>
-          </div>
-          <div class="subnav-level-2-items collapse show" id="collapseSubnav">
-            <li
-              v-for="childItem of submenuItem.children"
-              class="nav-item subnav-level-2-item"
-              :class="route.path.includes(childItem.link) ? 'selected' : ''"
-            >
-              <NuxtLink :to="childItem.link" class="nav-link"
-                >{{ childItem.title }}
-              </NuxtLink>
-              <ul
-                v-if="childItem.children.length > 0"
-                class="subnav-level-3-items nav"
-              >
-                <li
-                  v-for="grandchildItem of submenu[displayChildren].children"
-                  class="nav-item"
-                  :class="[
-                    {
-                      'current-page':
-                        grandchildItem == submenuItems[0].children[index],
-                    },
-                  ]"
+                  {{ submenuItem.title }}
+                </NuxtLink>
+              </li>
+              <li class="nav-item">
+                <button
+                  class="btn cbd-btn-subnavigation"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseSubnav"
+                  aria-expanded="true"
+                  aria-controls="collapseSubnav"
                 >
-                  <NuxtLink class="nav-link" :to="grandchildItem.link"
-                    >{{ grandchildItem.title }}
-                  </NuxtLink>
-                </li>
-              </ul>
-            </li>
-          </div>
-        </ul>
+                  <svg
+                    width="14"
+                    height="8"
+                    viewBox="0 0 14 8"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M12.92 0.180054H6.68999H1.07999C0.119992 0.180054 -0.360007 1.34005 0.319993 2.02005L5.49999 7.20005C6.32999 8.03005 7.67999 8.03005 8.50999 7.20005L10.48 5.23005L13.69 2.02005C14.36 1.34005 13.88 0.180054 12.92 0.180054Z"
+                    />
+                  </svg>
+                </button>
+              </li>
+            </div>
+            <div class="subnav-level-2-items collapse show" id="collapseSubnav">
+              <li
+                v-for="childItem of submenuItem.children"
+                class="nav-item subnav-level-2-item"
+                :class="fullPath.includes(childItem.link) ? 'selected' : ''"
+              >
+                <NuxtLink :to="childItem.link" class="nav-link"
+                  >{{ childItem.title }}
+                </NuxtLink>
+                <ul
+                  v-if="childItem.children.length > 0"
+                  class="subnav-level-3-items nav"
+                >
+                  <li
+                    v-for="grandchildItem of childItem.children"
+                    class="nav-item"
+                    :class="
+                      fullPath.includes(grandchildItem.link) ? 'selected' : ''
+                    "
+                  >
+                    <NuxtLink class="nav-link" :to="grandchildItem.link">{{
+                      grandchildItem.title
+                    }}</NuxtLink>
+                  </li>
+                </ul>
+              </li>
+            </div>
+          </ul>
+          <ul class="subnav-level-3-items nav">
+            <template v-for="childItem of submenuItem.children">
+              <li
+                v-for="grandchildItem of childItem.children"
+                class="nav-item"
+                :class="
+                  fullPath.includes(grandchildItem.link) ? 'selected' : ''
+                "
+              >
+                <NuxtLink class="nav-link" :to="grandchildItem.link">{{
+                  grandchildItem.title
+                }}</NuxtLink>
+              </li>
+            </template>
+          </ul>
+        </template>
       </ClientOnly>
     </nav>
   </div>
--- layouts/content/index.vue
+++ layouts/content/index.vue
@@ -1,5 +1,33 @@
 <script setup lang="ts">
+import type { fetchedMenuItem } from "~/types/drupalMenu";
+
 const route = useRoute();
+
+const submenuItems = ref<fetchedMenuItem[]>([]);
+
+if (referencedPage.value?.field_menu) {
+  await handlerSubmenuNavigation(referencedPage.value.field_menu);
+
+  const currentPath = route.path;
+  const fullPath = route.fullPath;
+
+  for await (const menuItem of submenu.value) {
+    for (const childItem of menuItem.children) {
+      if (currentPath.includes(childItem.link)) {
+        submenuItems.value.push(menuItem);
+      } else {
+        for (const grandchildItem of childItem.children) {
+          if (
+            currentPath.includes(grandchildItem.link) ||
+            fullPath.includes(grandchildItem.link)
+          ) {
+            submenuItems.value.push(menuItem);
+          }
+        }
+      }
+    }
+  }
+}
 </script>
 
 <template>
@@ -31,13 +59,14 @@ const route = useRoute();
         <HeroContent :article="referencedArticles" />
       </template>
     </ClientOnly>
-    <main class="cus-main d-flex flex-column" role="main">
+    <main
+      class="cus-main d-flex flex-column"
+      :class="route.meta.pageType === 'page' ? 'cus-internal-page' : ''"
+      role="main"
+    >
       <NavigationSubmenuHorizontal
-        v-if="
-          referencedPage?.field_menu &&
-          route.fullPath.includes(referencedPage.url)
-        "
-        :submenu-name="referencedPage.field_menu"
+        v-if="referencedPage && route.fullPath.includes(referencedPage.url)"
+        :submenu-items="submenuItems"
       />
       <NuxtPage :page-key="(route) => route.fullPath" />
     </main>
