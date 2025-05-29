16df5ce7a6b4f691dfe50a7c61d819a4e56633bb
RomeoSaundersII <romeo.saunders@gmail.com>
2025-05-29 11:31:43 -0400

    Fixed previously unresolved merge issue. Converted props to types.

--- components/navigation/megamenu/NavigationMegamenuDrawer.vue
+++ components/navigation/megamenu/NavigationMegamenuDrawer.vue
@@ -1,34 +1,26 @@
 <script setup lang="ts">
-    import type { fetchedMenuItem } from '~/types/drupalMenu';
-    
-    const props = defineProps<
-    {
-        parent: fetchedMenuItem
-    }
-    >();
+import type { fetchedMenuItem } from "~/types/drupalMenu";
+
+const props = defineProps<{
+  parent: fetchedMenuItem;
+}>();
 </script>
 
 <template>
-    <nav class="mega-menu-drawer dropdown-menu container-fluid">
-                    
-        <ul class="mega-menu-drawer-internal nav">
-            <li class="sections-header">Sections</li>
-            <li class="level-1-item nav-item">
-                <NuxtLink class="nav-link" :to="props.parent.link">
-                    {{ props.parent.title }}
-                </NuxtLink>
-            </li>
+  <nav class="mega-menu-drawer dropdown-menu container-fluid">
+    <ul class="mega-menu-drawer-internal nav">
+      <li class="sections-header">Sections</li>
+      <li class="level-1-item nav-item">
+        <NuxtLink class="nav-link" :to="parent.link">
+          {{ parent.title }}
+        </NuxtLink>
+      </li>
 
-            <div 
-                v-if="props.parent.children.length > 0"
-                class="level-2-items">
-                
-                <NavigationMegamenuLevel2 :parent="props.parent" />
-                
-            </div>
-        </ul>
+      <div v-if="parent.children.length > 0" class="level-2-items">
+        <NavigationMegamenuLevel2 :parent="parent" />
+      </div>
+    </ul>
 
-        <NavigationMegamenuSocialmedia />
-
-    </nav>
+    <NavigationMegamenuSocialmedia />
+  </nav>
 </template>
--- components/navigation/megamenu/NavigationMegamenuLevel3.vue
+++ components/navigation/megamenu/NavigationMegamenuLevel3.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
-    import type { fetchedMenuItem } from '~/types/drupalMenu';
-    
-    const props = defineProps<
-    {
-        parent: fetchedMenuItem
-    }
-    >();
+import type { fetchedMenuItem } from "~/types/drupalMenu";
+
+const props = defineProps<{
+  parent: fetchedMenuItem;
+}>();
 </script>
 
 <template>
-    <li 
-        v-for="level3_item in props.parent.children"
-        class="nav-item">
-        <NuxtLink class="nav-link" :to="level3_item.link">
-            {{ level3_item.title }}
-        </NuxtLink>
-    </li>
+  <li v-for="level3_item in parent.children" class="nav-item">
+    <NuxtLink class="nav-link" :to="level3_item.link">
+      {{ level3_item.title }}
+    </NuxtLink>
+  </li>
 </template>
