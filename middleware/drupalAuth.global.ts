60b6a8747f57e12b1cefaf23fa8f7c670723a4c7
RomeoSaundersII <romeo.saunders@gmail.com>
2025-07-21 15:05:09 -0400

    Force refresh page until refresh_token issue is resolved.

--- middleware/drupalAuth.global.ts
+++ middleware/drupalAuth.global.ts
@@ -1,19 +1,26 @@
-import { type drupalToken } from "~/types/drupalAuth"
+import { type drupalToken } from "~/types/drupalAuth";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  const drupalToken = useState<drupalToken | null>('drupal_token', () => null)
+  const drupalToken = useState<drupalToken | null>("drupal_token", () => null);
 
-  const tokenExpired = drupalToken.value !== null && 
-    drupalToken.value.timeout && 
-    drupalToken.value.timeout <= Date.now()
+  const tokenExpired =
+    drupalToken.value !== null &&
+    drupalToken.value.timeout &&
+    drupalToken.value.timeout <= Date.now();
 
-  if (!drupalToken.value || tokenExpired) {
+  if (!drupalToken.value) {
     try {
-      const tokenData = await getDrupalToken()
+      const tokenData = await getDrupalToken();
       drupalToken.value = tokenData;
-      console.log('Token valid until:', new Date(tokenData.timeout).toLocaleString())
+      console.log(
+        "Token valid until:",
+        new Date(tokenData.timeout).toLocaleString()
+      );
     } catch (error) {
-      console.error('Error:', error)
+      console.error("Error:", error);
     }
+  } else if (tokenExpired) {
+    // Temporary Fix until Drupal Refresh Token issue is resolved.
+    reloadNuxtApp({ force: true, persistState: true, path: to.path });
   }
-})
+});
